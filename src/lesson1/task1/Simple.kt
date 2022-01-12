b4a813a34c47867f0566c2e6898408a00e729aee
sergunadze <s_novikov@mail.ru>
2022-01-12 21:33:52 +0300

    Решил задачу thirdDigit

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -104,7 +104,11 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double {
  * Пользователь задает целое число, больше или равно 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int {
+    val result: Int = number / 100 % 10
+
+    return result
+}
 
 /**
  * Простая (2 балла)
