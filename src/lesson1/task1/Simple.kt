cbf88a4757d020f48f5f916751508b1aa7aafeee
sergunadze <s_novikov@mail.ru>
2022-01-12 21:44:46 +0300

    Решил задачу travelMinutes

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -117,7 +117,9 @@ fun thirdDigit(number: Int): Int {
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    return (hoursArrive*60+minutesArrive) - (hoursDepart*60+minutesDepart)
+}
 
 /**
  * Простая (2 балла)
